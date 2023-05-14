ae6335236f02f9e0052a60018d88cd9ede565e3f
Anton <antontikhomirov93@gmail.com>
2023-05-14 20:59:42 +0300

    Initial Components Commit

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import {ConnectDevice} from './pages/connect-device';
+
 export function App() {
-	return <h1>Hello world!</h1>;
+	return (
+		<>
+			<h1>Hello world!</h1>
+			<ConnectDevice />
+		</>
+	);
 }
---
+++ src/popup/pages/connect-device/ConnectDevice.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function ConnectDevice() {
+	return <h1>See QR here later!</h1>;
+}
