49ebccf1bbde29c1b53fa572c385631046642291
Anton <antontikhomirov93@gmail.com>
2023-05-14 12:13:56 +0300

    Add popup App.jsx

--- src/popup/index.js
+++ src/popup/index.js
@@ -1,20 +1,6 @@
-import React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const element = React.createElement(
-  'h1',
-  {className: 'greeting'},
-  'Hello, world!'
-);
-
-root.render(element);
-
-// const root = document.querySelector('#root');
-//
-// render(
-//   <React.StrictMode>
-//     {element}
-//   </React.StrictMode>,
-//   root,
-// );
+const container = document.getElementById("root");
+const root = createRoot(container)
+root.render(<App />);
