8a0aa81c6d6f06929ed6840471317536a0602220
Anton <antontikhomirov93@gmail.com>
2023-05-16 21:22:28 +0300

    Added socket-io

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,15 @@
+import {io} from 'socket.io-client';
+
+const socket = io('wss://127.0.0.1:3000');
+
 console.log('Background Script Run!');
+
+socket.on('connect', () => {
+	console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+});
+
+socket.send('TEST 123');
+
+socket.on('disconnect', () => {
+	console.log(socket.id); // undefined
+});
