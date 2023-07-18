124c4943fac9a941aad070888a4260c6262ba30b
Anton <antontikhomirov93@gmail.com>
2023-07-18 20:09:36 +0300

    Fix Root and Tabs

--- src/popup/routes/Root.tsx
+++ src/popup/routes/Root.tsx
@@ -1,7 +1,21 @@
 import React, {FC} from 'react';
+import {Outlet} from 'react-router-dom';
 
-export const Tabs: FC = () => (
+export const Root: FC = () => (
 	<div>
-      Tabs list
+		<h1>React Router Contacts</h1>
+		<nav>
+			<ul>
+				<li>
+					<a href={'/contacts/1'}>Link 1</a>
+				</li>
+				<li>
+					<a href={'/contacts/2'}>Link 2</a>
+				</li>
+			</ul>
+		</nav>
+		<div>
+			<Outlet />
+		</div>
 	</div>
 );
--- src/popup/routes/Tabs.tsx
+++ src/popup/routes/Tabs.tsx
@@ -1,21 +1,7 @@
 import React, {FC} from 'react';
-import {Outlet} from 'react-router-dom';
 
-export const Root: FC = () => (
+export const Tabs: FC = () => (
 	<div>
-		<h1>React Router Contacts</h1>
-		<nav>
-			<ul>
-				<li>
-					<a href={'/contacts/1'}>Link 1</a>
-				</li>
-				<li>
-					<a href={'/contacts/2'}>Link 2</a>
-				</li>
-			</ul>
-		</nav>
-		<div>
-			<Outlet />
-		</div>
+      Tabs list
 	</div>
 );
