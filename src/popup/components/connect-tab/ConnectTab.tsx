05a53af34a417e6c7918291c7be6017537b00133
Anton <antontikhomirov93@gmail.com>
2023-07-28 20:59:55 +0300

    Add QR code component

--- src/popup/components/connect-tab/ConnectTab.tsx
+++ src/popup/components/connect-tab/ConnectTab.tsx
@@ -1,14 +1,34 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {useParams} from 'react-router-dom';
+import {useTranslation} from 'react-i18next';
+import QRCode from 'qrcode';
 
 import s from './styles.module.scss';
 
 export const ConnectTab: FC = () => {
+	const {t} = useTranslation();
 	const {tabId} = useParams();
+	const [qrDataUrl, setQrDataUrl] = useState<string | undefined>();
+	const [qrError, setQrError] = useState<string | undefined>();
+
+	console.log(tabId);
+
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+		QRCode.toDataURL(tabId.toString())
+			.then(data => {
+				setQrDataUrl(data);
+			})
+			.catch(() => {
+				setQrError(t('common:connect-tab-qr-error'));
+			});
+	}, []);
 
 	return (
-		<div className={s.tab}>
-			<span className={s.title}>Tab Id: {tabId}</span>
+		<div className={s.container}>
+			<span className={s.tutorial}>{t('common:connect-tab-qr-tutorial')}</span>
+			{qrError && <span className={s.error}>{qrError}</span>}
+			{qrDataUrl && <img src={qrDataUrl} />}
 		</div>
 	);
 };
